241759249d31189ba27a2b69816c039fe699581e
Sharanu Mesta <sharanumesta1201@gmail.com>
2025-03-09 09:31:43 +0530

    Bug fixes

--- src/controllers/school.controller.js
+++ src/controllers/school.controller.js
@@ -20,27 +20,29 @@ const addSchool = async (req, res) => {
       [id, name, address, latitude, longitude]
     );
 
-    res
-      .status(201)
-      .json({
-        message: "School data has been added successfully",
-        insertId: insertResult.insertId,
-      });
+    res.status(201).json({
+      message: "School data has been added successfully",
+    });
   } catch (error) {
-    res.status(500).json({ message: "Error while adding school data", error: error.message });
+    res
+      .status(500)
+      .json({
+        message: "Error while adding school data",
+        error: error.message,
+      });
   }
 };
 
 const haversine = (lat1, lon1, lat2, lon2) => {
-  const R = 6371; // Radius of Earth in km
+  const R = 6371;
   const dLat = ((lat2 - lat1) * Math.PI) / 180;
   const dLon = ((lon2 - lon1) * Math.PI) / 180;
   const a =
     Math.sin(dLat / 2) * Math.sin(dLat / 2) +
     Math.cos((lat1 * Math.PI) / 180) *
-      Math.cos((lat2 * Math.PI) / 180) *
-      Math.sin(dLon / 2) *
-      Math.sin(dLon / 2);
+    Math.cos((lat2 * Math.PI) / 180) *
+    Math.sin(dLon / 2) *
+    Math.sin(dLon / 2);
   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
   const distance = R * c; // Distance in km
   return distance;
